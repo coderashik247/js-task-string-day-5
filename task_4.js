a5d08824b2cc4203b5698b11759cd322be963915
Ashik <ashikurashik20999@gmail.com>
2024-09-07 23:09:37 +0600

    task_3 added

---
+++ task_4.js
@@ -0,0 +1,12 @@
+//hint: You should be able to check whether x or X exists. After that, search online how to replace a character in a string.
+
+// let str = "Hello";
+
+// str = str.replace('H', 'h');
+// console.log(str);
+
+let str = "You should be able to check whether x or X exists. After that, search online how to replace a character in a string.";
+
+str = str.replace('x','y');
+str = str.replace('X', 'Y');
+console.log(str);
